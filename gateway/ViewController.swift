377b6ed7ecc1f79365e4fb1d190e5daad40edb4b
hmk20000 <hmk20000@nate.com>
2016-09-19 13:12:57 +0900

    Initial Commit

---
+++ gateway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gateway
+//
+//  Created by ming on 2016. 9. 19..
+//  Copyright © 2016년 cccvlm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
